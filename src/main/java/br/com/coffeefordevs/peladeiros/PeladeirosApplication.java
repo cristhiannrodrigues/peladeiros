affe8611dab64e0617e05ce36f4b7d0da742ba49
Cristhiann Rodrigues <cristhiannrodrigues@MacBook-Air-de-Cristhiann.local>
2022-12-20 19:40:45 -0300

    Initializing Project

---
+++ src/main/java/br/com/coffeefordevs/peladeiros/PeladeirosApplication.java
@@ -0,0 +1,13 @@
+package br.com.coffeefordevs.peladeiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeladeirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeladeirosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/coffeefordevs/peladeiros/PeladeirosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coffeefordevs.peladeiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeladeirosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

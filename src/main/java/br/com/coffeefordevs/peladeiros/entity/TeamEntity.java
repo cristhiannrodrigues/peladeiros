541a2a6c5873e032092ae1749f26e5c27076e3d9
cristhiannsingular <cristhiann.martins@opensingular.com>
2022-12-28 10:47:53 -0300

    Creating Team endpoints

--- src/main/java/br/com/coffeefordevs/peladeiros/entity/TeamEntity.java
+++ src/main/java/br/com/coffeefordevs/peladeiros/entity/TeamEntity.java
@@ -15,6 +15,10 @@ public class TeamEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    public TeamEntity(String name) {
+        setName(name);
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
